b8141c85e45d17d8e88b9c2b11b128ab5a337027
afkarxyz <muhammadzamzamafkar@gmail.com>
2025-11-07 07:20:59 +0700

    .

---
+++ chatgpt-tts.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name            ChatGPT TTS | Text-to-Speech Generator & Audio Downloader
+// @description     Text-to-Speech generator and audio downloader for ChatGPT.
+// @namespace       https://exyezed.cc
+// @version         1.0.2
+// @author          exyezed
+// @license         MIT
+// @supportURL      https://exyezed.cc
+// @homepageURL     https://exyezed.cc
+// @updateURL       https://github.com/exyezed/chatgpt-tts/releases/latest/download/chatgpt-tts.meta.js
+// @downloadURL     https://github.com/exyezed/chatgpt-tts/releases/latest/download/chatgpt-tts.user.js
+// ==/UserScript==
